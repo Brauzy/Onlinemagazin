d888fa2e17f7bf480c925b03906ee8f368de74c5
Kaloyan Ivanov <cdevyt@gmail.com>
2022-03-29 22:33:21 +0300

    Update files. data.json by Hristian.

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Card from './Card';
+import List from './List';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      
+      <List></List>
       <StatusBar style="auto" />
     </View>
   );
---
+++ List.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+import {FlatList} from 'react-native';
+import Card from './Card'
+import {Text} from 'react-native';
+
+
+class List extends Component{
+    state = {
+        items: []
+    }
+
+    componentDidMount() {
+        const items = require('./data.json').items;
+        this.setState( {items} );
+    }
+    render() {
+        return(
+            <FlatList
+                data = {this.state.items}
+                renderItem = { ({item}) => <Card item = {item} />}
+            ></FlatList>
+        );
+    };
+};
+
+export default List;
